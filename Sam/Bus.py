b275b3a0acef6b1fc42f7f48e964d6c88aaf675d
Erin Rai <raie@uni.coventry.ac.uk>
2014-12-08 14:57:04 +0000

    Business Profit/Loss Calc

---
+++ Sam/Bus.py
@@ -0,0 +1,22 @@
+oi = input("Enter Opening Inventories: ")
+p = input("Enter Purchases: ")
+ci = input("Enter Closing Inventories: ")
+sr = input("Enter Sales Revenue: ")
+
+result_cos = oi + p - ci
+cos = result_cos
+result_gp = sr - cos
+
+
+def cos(oi, p, ci):
+    oi
+    p
+    ci
+    print result_cos
+
+def gp(sr, cos):
+    print result_gp
+
+
+cos(0, 0, 0)
+gp(0, 0)

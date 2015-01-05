8d8dd81540d887027affc0ce896f156360af0749
Saikou Bojang <bojangs@uni.coventry.ac.uk>
2015-01-05 15:42:57 +0000

    csv
    csv

--- Sai 5761900/csv files.py
+++ Sai 5761900/csv files.py
@@ -1,55 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print "Welcome to the Data Processing Tool"
-x = raw_input("To continue, type 'ENTER': ")
-if x == ("ENTER"):
-    print " "
-else:
-    quit()
-
-print "(Example username = 'User', Example Password = 'Pass')"
-
-Words = ("John", "Sam", "Tom", "Sai", "Samira", "User", "U")
-isinlist = False
-
-while True:
-    x = raw_input ("Username: ")
-    for word in Words:
-        if x == word:
-            isinlist = True
-    if isinlist == True:
-        break
-
-Words = ("Password123", "Password", "Pass", "P")
-isinlist = False
-
-while True:
-    x = raw_input ("Password: ")
-    for word in Words:
-        if x == word:
-            isinlist = True
-    if isinlist == True:
-        break
-
-print "Welcome!"
-print " "
-
 import urllib2
 import json
 
@@ -116,8 +64,4 @@ for x in range(5000):
         print "Opening Stock:            £" + str(data[x]['company']['opening_stock'])
         print "Closing Stock:            £" + str(data[x]['company']['closing_stock'])
 
-
-        
-        
-
-
+data.save("trial.Csv")

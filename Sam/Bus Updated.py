1d5300c705eace3cdca1df0938f038db9a442817
Erin Rai <raie@uni.coventry.ac.uk>
2015-01-07 12:36:44 +0000

    Bus Updated

---
+++ Sam/Bus Updated.py
@@ -0,0 +1,24 @@
+oi = input("Enter Opening Inventories: ")
+p = input("Enter Purchases: ")
+ci = input("Enter Closing Inventories: ")
+sr = input("Enter Sales Revenue: ")
+
+result_cos = oi + p - ci
+cos = result_cos
+result_gp = sr - cos
+
+
+def cos(oi, p, ci):
+    oi
+    p
+    ci
+    print "Cost of Sales:"
+    print int(result_cos)
+
+def gp(sr, cos):
+    print "Gross Profi:"
+    print result_gp
+
+
+cos(0, 0, 0)
+gp(0, 0)
